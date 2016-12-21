05ef72615f4f8ed6db370f20365787e77c711e22
izznadjmi <izz_nadjmi@yahoo.com>
2016-12-21 22:41:26 +0800

    Updated GUI for Map View App
    Updated the GUI for map viewing app

--- src/com/SWM/MapViewer/ViewApp/Viewer_Controller.java
+++ src/com/SWM/MapViewer/ViewApp/Viewer_Controller.java
@@ -1,5 +1,25 @@
 package com.SWM.MapViewer.ViewApp;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+
 public class Viewer_Controller {
+	@FXML
+	AnchorPane base;
 	
+	@FXML
+	MenuItem ExitMenu;
+	
+	@FXML
+	GridPane grid;
+	
+	@FXML
+	Button setaxeloc;
+	
+	@FXML
+	Button setboatloc;
+	
+	@FXML
+	Button rundh;
 }
