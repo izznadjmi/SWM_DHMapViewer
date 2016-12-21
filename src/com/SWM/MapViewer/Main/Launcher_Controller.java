53af3881ea4bdca1d18f4532fb999198c1714cd7
amnanfauzi <amnanfauzi@gmail.com>
2016-12-21 22:44:49 +0800

    Updated Controller for Launcher
    Set launch view button to launch the map view app.

--- src/com/SWM/MapViewer/Main/Launcher_Controller.java
+++ src/com/SWM/MapViewer/Main/Launcher_Controller.java
@@ -3,7 +3,11 @@ package com.SWM.MapViewer.Main;
 import com.neet.DiamondHunter.Main.Game;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class Launcher_Controller {
 	@FXML
@@ -16,5 +20,26 @@ public class Launcher_Controller {
 	{
 		Game nw = new Game();
 		nw.RunGame();
+	}
+	
+	public Stage secondaryStage;
+	
+	public void Launch_Viewer()
+	{
+		try 
+		{
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/SWM/MapViewer/ViewApp/MapViewer.fxml"));
+			AnchorPane root = (AnchorPane) fxmlLoader.load();
+			Scene scene = new Scene(root, 840, 665);
+			secondaryStage = new Stage();
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			secondaryStage.setTitle("Map Viewer");
+			secondaryStage.setScene(scene);
+			secondaryStage.show();
+		} 
+		catch(Exception e) 
+		{
+			e.printStackTrace();
+		}
 	}
 }
